5d24d72ceca87c5d0888ff3103fc6951de050f9b
bandura <banduraweb@ukr.net>
2020-02-03 06:47:02 +0200

    heroku serv add

--- client/src/vuex/store.js
+++ client/src/vuex/store.js
@@ -15,7 +15,7 @@ let store = new Vuex.Store({
     },
     actions: {
         GET_PRODUCTS_FROM_API({commit}) {
-            return axios('http://localhost:4444/products', {
+            return axios('https://damp-sierra-65910.herokuapp.com/products/', {
                 method: "GET"
             })
                 .then((products) => {
